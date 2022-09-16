c55c266c2739fe5f4adf818f03be5c9dd352b546
bestload <egor.panyushkin@gmail.com>
2022-09-16 21:23:29 +0300

    initial commit

---
+++ src/header.h
@@ -0,0 +1,14 @@
+/*
+    diff: easy
+
+    Given a string s, reverse the order of characters in each word within 
+    a sentence while still preserving whitespace and initial word order.
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverseWords(string s);
