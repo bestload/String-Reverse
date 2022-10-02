ec3aae594bfe8946a85f7694d0a36e9ac24e9232
bestload <egor.panyushkin@gmail.com>
2022-10-02 16:00:43 +0300

    ADD/UPD: MergeSort, sortedSquares

--- src/header.h
+++ src/header.h
@@ -7,7 +7,8 @@
 */
 
 #include <iostream>
-#include <string>
+#include <vector>
+
 
 using namespace std;
 
--- src/source.cpp
+++ src/source.cpp
@@ -1,5 +1,44 @@
 #include "header.h"
 
-    string reverseWords(string s) {
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> sum;
+        for(auto it = numbers.begin(); it != numbers.end(); ++it)
+        {
+            for(auto jt = numbers.begin() + 1; it != numbers.end(); ++jt)
+            {
+                
+            }
+        }
+    }
+
+    // void reverseString(vector<char>& s) 
+    // {
+    //     /*
+    //         Этапы: 
+    //             1. Проверить строку на пустоту
+    //             2. Создать вторую строку
+    //             3.    
         
+        
+    //     */
+
+    //     vector<int> n;
+        
+        
+    //     short i = 0;
+    //     char c_start = 0;
+    //     char c_end = 0;
+        
+    //     if(nullptr == &s)
+    //     {
+    //         return nullptr;
+    //     }
+    //     while(i != ((s.size() / 2) + 1))
+    //     {
+    //         c_start = s[i];
+    //         c_end = s[s.size() - i];
+    //         s[i] = c_end;
+    //         s[s.size() - i] = c_start;
+    //         ++i;
+    //     }
     }
